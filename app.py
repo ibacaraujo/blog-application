1ca8da226befd023d3aaf29815cc3ebbedd25b02
Iago Breno Araujo <ibacaraujo@gmail.com>
2019-05-04 11:57:36 -0300

    Add register page to the application

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run()
