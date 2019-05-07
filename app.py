5d1658430e23d25a2a8e897b65224b4a80fc8dcf
Iago Breno <ibacaraujo@gmail.com>
2019-05-07 09:53:17 -0300

    Handle register request

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -8,7 +9,7 @@ Bootstrap(app)
 # Configure db
 db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['myql_user']
+app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
@@ -19,6 +20,16 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        form = request.form
+        name = form['name']
+        email = form['email']
+        age = form['age']
+        password = form['pass']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO user(name, email, age, password) VALUES(%s, %s, %s, %s)",
+            (name, age, age, password))
+        mysql.connection.commit()
     return render_template('register.html')
 
 @app.route('/login')
